025950937a75691e8ef0f05f48f11e1957c56123
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2023-02-04 11:26:17 +0200

    create brands section

--- src/containers/whatAynAcademy/WhatAynAcademy.js
+++ src/containers/whatAynAcademy/WhatAynAcademy.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Feature } from '../../components'
 import './whatAynAcademy.css'
 
 const WhatAynAcademy = () => {
   return (
-    <div>WhatAynAcademy</div>
+    <div className='ayn__whataynacademy section__margin' id='whatayn'>
+      <div className="ayn__whataynacademy-feature">
+        <Feature />
+      </div>
+      <div className="ayn_whataynacademy-heading">
+        <h1 className='gradient-text'>
+          فرص التطور والتعلم معنا أكبر مما تتخيل
+        </h1>
+        <p>تصفح المكتبة</p>
+      </div>
+      <div className="ayn__whataynacademy-container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+    </div>
   )
 }
 
