248c8f1d9ff70871deb2dc46df02589445c86d87
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2023-11-16 02:36:13 +0200

    remove the files and start over with typescript

---
+++ ayn_backend/index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import "dotenv/config";
+
+import { apiErrorHandler} from './middlewares/apiErrorHandler';
+import { routeNotFound } from './middlewares/routeNotFound';
+
+const app = express();
+const PORT = 6000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Connect to MongoDB
+const mongoURL = process.env.MONGO_URI as string;
+mongoose.connect(mongoURL).then(() => console.log("Connected!"));
+
+app.use(apiErrorHandler);
+app.use(routeNotFound);
+app.listen(PORT, () => {
+  console.log(`👀 Server is running on localhost:${PORT}`);
+});
