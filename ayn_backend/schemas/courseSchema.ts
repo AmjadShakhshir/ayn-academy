af8638993f95b9a8ef36c7bc65752e0b18a8d198
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2023-11-19 14:02:44 +0200

    add course schema

---
+++ ayn_backend/schemas/courseSchema.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+import mongoose from "mongoose";
+
+export const courseBodySchema = z.object({
+    name: z.string().min(3).max(20),
+    code: z.string().min(3).max(20),
+    description: z.string().min(3).max(20),
+    userId: z.string().refine((val) => mongoose.Types.ObjectId.isValid(val)),
+}).strict();
+
+export const updatedCourseBodySchema = z.object({
+    name: z.string().min(3).max(20).optional(),
+    code: z.string().min(3).max(20).optional(),
+    description: z.string().min(3).max(200).optional(),
+    userId: z.string().refine((val) => mongoose.Types.ObjectId.isValid(val)).optional(),
+}).partial().strict();
+
+export const courseSchema = z.object({
+    body: courseBodySchema,
+});
+
+export const updateCourseSchema = z.object({
+    body: updatedCourseBodySchema,
+});
