64499efdc64bff3a5f465f863a1834acaeec8598
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2023-11-26 12:05:41 +0200

    setup test jest files

---
+++ ayn_backend/test/db-helper.ts
@@ -0,0 +1,28 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+
+async function connect(){
+    const mongd = await MongoMemoryServer.create();
+    const uri = mongd.getUri();
+
+    await mongoose.connect(uri);
+
+    return {
+        closeDatabase: async () => {
+            await mongoose.connection.dropDatabase();
+            await mongoose.connection.close();
+
+            await mongd.stop();
+        },
+        clearDatabase: async () => {
+            const collections = mongoose.connection.collections;
+
+            for(const key in collections){
+                const collection = collections[key];
+                await collection.deleteMany({});
+            }
+        }
+    }
+}
+
+export default connect;
---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
+    globals: {
+        "ts-jest": {
+            tsconfig: "tsconfig.json",
+        },
+    },
+    moduleFileExtensions: ["ts", "js"],
+    transform: {
+        "^.+\\.(ts|tsx)$": "ts-jest",
+    },
+    testMatch: ["**/tests/**/*.test.(ts|js)"],
+    testEnvironment: "node",
+    setupFiles: ["dotenv/config"],
+};
+
+export default config;
