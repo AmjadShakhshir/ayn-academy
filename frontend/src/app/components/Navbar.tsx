86c46256c4faea3b8e6ecd723f4f6806ce45dccb
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-02-14 19:02:50 +0200

    rebuild navbar

--- frontend/src/app/components/Navbar.tsx
+++ frontend/src/app/components/Navbar.tsx
@@ -1,92 +1,142 @@
 import * as React from 'react';
-import { AppBar, Box, Toolbar, IconButton, Typography, Menu, Button, MenuItem } from '@mui/material'; 
-import { Link } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
 import { images } from '../constants';
-import { logout, reset } from '../../features/auth/authService';
-import { useNavigate } from 'react-router-dom';
-import useAppDispatch from '../hooks/useAppDispatch';
-import useAppSelector from '../hooks/useAppSelector';
-import { useState } from 'react';
 
-const pages = [
-    { name: 'الرئيسية', path: '/' },
-    { name: 'من نحن ؟', path: '/who' },
-    { name: 'الدروس', path: '/courses' },
-    { name: 'المدونة', path: '/blog' },
-    { name: 'المنتدى', path: '/forum' },
-    { name: 'اتصل بنا', path: '/contact' },
-];
+const pages = ['الرئيسية', 'من نحن؟', 'المميزات', 'الفرص', 'الدروس', 'عن الدورة'];
+const settings = ['الملف الشخصي', 'الحساب', 'لوحة التحكم', 'الخروج'];
 
-type PageType = {
-    name: string;
-    path: string;
-};
+function ResponsiveAppBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+  const [logged, setLogged] = React.useState(false);
 
-function Navbar() {
-    const dispatch = useAppDispatch();
-    const navigate = useNavigate();
-    const { currentUser } = useAppSelector((state) => state.auth);
-    const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
-    const handleOpenNavMenu = (event: React.MouseEvent<HTMLButtonElement>) => setAnchorElNav(event.currentTarget);
-    const handleCloseNavMenu = () => setAnchorElNav(null);
-    const onLogout = () => {
-        dispatch(logout());
-        dispatch(reset());
-        navigate('/');
-    };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
-    const renderMenuItem = (page: PageType) => (
-        <MenuItem key={page.name} onClick={() => handleCloseNavMenu}>
-            <Typography textAlign="center">{page.name}</Typography>
-        </MenuItem>
-    );
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
-    const renderButton = (page: PageType) => (
-        <Button key={page.name} onClick={()=> handleCloseNavMenu} sx={{ my: 2, fontFamily: 'sans-serif', fontWeight: 700, color: 'white', display: 'block', fontSize: '1rem' }}>
-            <Link style={{ color: 'black' }} to={page.path}>{page.name}</Link>
-        </Button>
-    );
+  return (
+    <AppBar position="static" color='transparent' elevation={0} className='nav scale-up-center'>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <img src={images.logo} alt="logo" className='nav-logo' />
 
-    return (
-            <AppBar position="static">
-                    <Toolbar disableGutters sx={{
-                        display: 'flex',
-                        justifyContent: 'space-between',
-                        alignItems: 'center',
-                        padding: '0 5%',
-                        bgcolor: '#ecfcf0'
-                    }}>
-                        <Box sx={{ flexGrow: 0, pl:'20px' }}>
-                            <Link to="/">
-                                <img src={images.logo01} style={{ width: 100, height: 50 }} alt="logo" />
-                            </Link>
-                        </Box>
-                        <Box sx={{ flexGrow: 1, display: { md: 'flex', lg: 'none' } }}>
-                            <IconButton size="large" aria-label="account of current user" aria-controls="menu-appbar" aria-haspopup="true" onClick={handleOpenNavMenu} color="inherit">
-                                <MenuIcon />
-                            </IconButton>
-                            <Menu id="menu-appbar" anchorEl={anchorElNav} anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }} keepMounted transformOrigin={{ vertical: 'top', horizontal: 'left' }} open={Boolean(anchorElNav)} onClose={handleCloseNavMenu} sx={{ display: { md: 'block', lg: 'none' } }}>
-                                {pages.map(renderMenuItem)}
-                            </Menu>
-                        </Box>
-                        <Box sx={{ flexGrow: 1, display: { xs: 'none', lg: 'flex' } }}>
-                            {pages.map(renderButton)}
-                        </Box>
-                        <Box sx={{ flexGrow: 0, mx:'10px', display: 'flex' }}>
-                            {currentUser ? (
-                                <Button variant='contained' size='medium'  sx={{ mx: '10px', color:'#ffcc80' }} onClick={onLogout}>تسجيل الخروج</Button>
-                            ) : (
-                                <>
-                                    <Button variant='contained' size='medium'  sx={{ mx: '10px', display: { xs: 'none', md: 'block' }, backgroundColor:'#ffcc80' }}><Link style={{color: 'black'}} to="/login">تسجيل الدخول</Link></Button>
-                                    <Button variant='contained' size='medium' sx={{ display: { xs: 'none', md: 'block' }, backgroundColor:'#cc8a65'}} ><Link to="/register" style={{color: 'black'}}>انضم إلينا</Link></Button>
-                                </>
-                            )}
-                        </Box>
-                    </Toolbar>
-            </AppBar>
-    );
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                <Typography className='nav-item'>
+                  {page}
+                </Typography>
+              </Button>
+            ))}
+          </Box>
+          { !logged &&
+            <Box sx={{ flexGrow: 0 }}>
+              <button
+              className='btn-secondary'
+              onClick={() => setLogged(true)}
+              >تسجيل الدخول
+              </button>
+              <button
+              className='btn-primary'
+              onClick={() => setLogged(true)}
+              >انشاء حساب</button>
+            </Box>
+          }
+          { logged && (
+            <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>)}
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
-
-export default Navbar;
+export default ResponsiveAppBar;
