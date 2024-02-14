fc61cbd13451ee63fcf50a36684370fcea75fe8c
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-02-14 19:01:50 +0200

    rebuild why page

--- frontend/src/app/containers/Why.tsx
+++ frontend/src/app/containers/Why.tsx
@@ -1,13 +1,11 @@
-import { Box, Grid, Link, Typography } from '@mui/material'
+import { Box, Container, Grid, Link, Typography } from '@mui/material'
 
 const Why = () => {
   return (
-    <Box>
+    <Box className="why">
+        <Container maxWidth="xl" className='why-container'>
         {/* Section 1 */}
-        <Box component="section" sx={{
-            p: 12,
-            backgroundColor:'#fcfcfc'
-        }}>
+        <Box component="section">
             <Grid
             container
             spacing={2}
@@ -16,13 +14,11 @@ const Why = () => {
             alignItems="center"
             borderRadius={'22px'}>
                 <Grid item xs={12} md={6} >
-                    <Typography variant='h3' borderTop={'5px solid #b9f6ca'}
-                    sx={{
-                        width: '200px',
-                    }}> لماذا عين !؟</Typography>
+                    <Typography variant='h4'
+                    className='why-span'> لماذا عين !؟</Typography>
                 </Grid>
-                <Grid item xs={12} md={6} >
-                    <Typography variant='body2'> 
+                <Grid item xs={12} md={6}>
+                    <Typography variant='body2' className='why-subtitle'> 
                         في عالمٍ مليءٍ بالنداءات والدعوات صاخبٍ بالضجيج والتيارات تشغلنا فيه الحيرة والتساؤلات نحتاج إلى عين... عينٍ صافيةٍ عذبةٍ ننهل منها لنروي ظمأ أسئلتنا، عن الوجود والحياة، عن دورنا فيها، عن السعادة ما هي وكيف نجنيها !؟ عن الإنسان ما هو !؟ وإلى أين يسير؟
                     </Typography>
                 </Grid>
@@ -33,7 +29,7 @@ const Why = () => {
                         borderRadius: '22px',
                         paddingX: '10px',
                     }}>
-                    <Typography variant='h2'>
+                    <Typography variant='h2' className='why-title'>
                         فرص التطور والتعلم معنا أكبر مما تتخيل
                     </Typography>
                     </Grid>
@@ -41,7 +37,7 @@ const Why = () => {
                         <Link
                         variant='subtitle1'
                         href='/courses'
-                        color={'#77adcb'}
+                        className='why-link'
                         underline='hover'
                         >
                                 تصفح المكتبة
@@ -52,47 +48,36 @@ const Why = () => {
                     <Grid item xs={12} md={4} paddingLeft={3}>
                         <Typography
                         variant='h4'
-                        borderTop={'5px solid #b9f6ca'}
-                        sx={{
-                            width: '100px',
-                            paddingY: '5px',
-                        }}>
+                        className='why-span-1'>
                             الإبداع
                         </Typography>
-                        <Typography variant='body2'>
+                        <Typography variant='body2' className='why-subtitle'>
                             مناهج منصة عين هي مناهج أصيلة، قام فريق المنصة بتأليفها وتطويرها وتحويلها إلى دورات ونشاطات وألعاب بما يحقق الفائدة والمتعة للطلاب/المتدربين.
                         </Typography>
                         </Grid>
                     <Grid item xs={12} md={4} paddingLeft={3}>
                         <Typography variant='h4'
-                        borderTop={'5px solid #b9f6ca'}
-                        sx={{
-                            width: '100px',
-                            paddingY: '5px',
-                        }}>
+                        className='why-span-1'>
                             العملية
                         </Typography>
-                        <Typography variant='body2'>
+                        <Typography variant='body2' className='why-subtitle'>
                             لا يوجد شيء من مناهج ودورات منصة عين ليس له تطبيق عملي، بل هي متفاعلة بشكل تام مع الواقع لفهمه بشكل أفضل وللتعامل معه ولتغييره.
                         </Typography>
                     </Grid>
                     <Grid item xs={12} md={4}>
                         <Typography
                         variant='h4'
-                        borderTop={'5px solid #b9f6ca'}
-                        sx={{
-                            width: '100px',
-                            paddingY: '5px',
-                        }}>
+                        className='why-span-1'>
                             المنهجية
                         </Typography>
-                        <Typography variant='body2'>
+                        <Typography variant='body2' className='why-subtitle'>
                             شبر من معرفة منهجية خير من بحر من المعلومات المبعثرة، لذلك نركز في المنصة على أن تقدم المناهج معرفة حقيقية تساعد في تنظيم العقل وتزيد فاعليته، لا تشتته ولا توهمه بمعرفة مزيفة.
                         </Typography>
                     </Grid>
                 </Grid>
             </Grid>
         </Box>
+        </Container>
     </Box>
   )
 }
