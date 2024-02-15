8d42c767fcef9bda68dd9b3b80c1bd63bc209a05
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-02-15 17:30:28 +0200

    add new components to home and fix scss files

--- frontend/src/app/pages/Home.tsx
+++ frontend/src/app/pages/Home.tsx
@@ -3,6 +3,8 @@ import Why from "../containers/Why";
 import Future from "../containers/Future";
 import CoursesFeatures from "../containers/CoursesFeatures";
 import Cta from "../containers/CTA";
+import Possibility from "../containers/Possibility";
+import Schedule from "../containers/Schedule";
 
 const Home = () => {
   return (
@@ -10,8 +12,10 @@ const Home = () => {
         <Brands />
         <Why />
         <Future />
+        <Possibility />
         <Cta />
         <CoursesFeatures />
+        <Schedule />
     </>
   )
 }
